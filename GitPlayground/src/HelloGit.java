b8e5fcec40a6727c3b2884b511892bf2f43bd861
karanmalik13 <60868255+karanmalik13@users.noreply.github.com>
2020-03-07 14:50:45 +0530

    3

--- GitPlayground/src/HelloGit.java
+++ GitPlayground/src/HelloGit.java
@@ -1,14 +1,12 @@
 
 public class HelloGit {
-
+	
 	public static void main(String[] args) {
 			System.out.println("\t\tTHIS IS A GIT EXAMPLE");
 	
-	
-	
-	
-	
-	
+			
+			
+			System.out.println("NEW CHNGE CHECK");
 	}
 
 }
